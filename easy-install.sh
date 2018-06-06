0aaaeff3bf167debe9a801b714a8083205b9357e
Bajpan Gosh <bajpangosh@users.noreply.github.com>
2018-06-06 16:09:08 +0530

    Update easy-install.sh

--- easy-install.sh
+++ easy-install.sh
@@ -24,7 +24,7 @@ sudo apt-get install -y build-essential git python-software-properties
 echo 'installing Node Js';
 sleep 2;
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash  -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
